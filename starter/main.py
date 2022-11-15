fb975430ceeb3bac249dea82b1dc13a96176bca7
Rainer-Anton Englisch <rainerenglisch@users.noreply.github.com>
2022-11-15 09:23:48 +0100

    Update main.py

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,8 @@
 # Put the code for your API here.
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
