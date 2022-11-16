d5d5f916d5cac81cb52db1d4743a0f67b9bbd3e4
Rainer Englisch <rainer-anton.englisch@telekom.de>
2022-11-16 17:19:32 +0100

    tests

---
+++ starter/starter/test/test_model.py
@@ -0,0 +1,70 @@
+import pickle
+
+import pandas as pd
+import pytest
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.exceptions import NotFittedError
+
+from ..ml.data import process_data
+from ..ml.model import compute_model_metrics, train_model, inference
+
+
+@pytest.fixture
+def my_data():
+    data = pd.read_csv('../../data/census_clean.csv')
+    return data
+
+@pytest.fixture
+def my_splitted_data(my_data):
+    train, test = train_test_split(my_data, test_size=0.20)
+    return train, test
+
+@pytest.fixture
+def my_processed_data(my_splitted_data):
+    train, test = my_splitted_data
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+        ]
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    X_test, y_test, _, _ = process_data(
+        test, categorical_features=cat_features, label="salary", encoder=encoder, lb=lb, training=False
+    )
+    return X_train, y_train, X_test, y_test
+
+def test_processed_data_dims(my_processed_data):
+    X_train, y_train, X_test, y_test = my_processed_data
+    assert X_train.ndim == X_test.ndim
+    assert y_test.ndim == 1 and y_test.ndim == 1
+
+@pytest.fixture
+def my_model(my_processed_data):
+    X_train, y_train, X_test, y_test = my_processed_data
+    model = train_model(X_train, y_train)
+    return model
+
+def test_if_model_fitted(my_model, my_processed_data):
+    X_train, y_train, X_test, y_test = my_processed_data
+    error_occured=False
+    try:
+        my_model.predict(X_test)
+    except NotFittedError as e:
+        error_occured=True
+    assert error_occured==False
+
+
+def test_on_good_metrics(my_model, my_processed_data):
+    X_train, y_train, X_test, y_test = my_processed_data
+    preds = inference(my_model, X_test)
+    precision, recall, fbeta = compute_model_metrics(y_test, preds)
+    print(f'precision={precision:.2f}, recall={recall:.2f}, fbeta={fbeta:.2f}')
+    assert precision>0.7 and recall>0.6 and fbeta>0.6
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,11 +1,14 @@
 # Script to train machine learning model.
 
-from sklearn.model_selection import train_test_split
-
 # Add the necessary imports for the starter code.
-
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference
 # Add code to load in the data.
-
+data = pd.read_csv('./starter/data/census_clean.csv')
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
@@ -22,7 +25,21 @@ cat_features = [
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", encoder=encoder, lb=lb, training=False
+)
 
-# Proces the test data with the process_data function.
+#df_describe = pd.DataFrame(y_train)
+#print(df_describe.iloc[:,0].unique())
+# Process the test data with the process_data function.
 
 # Train and save a model.
+print('Train model')
+model = train_model(X_train, y_train)
+print('Compute metrics on test set')
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+print(f'precision={precision:.2f}, recall={recall:.2f}, fbeta={fbeta:.2f}')
+filename = './starter/model/model.pickle'
+print(f'Save model to {filename}')
+pickle.dump(model, open(filename, 'wb'))
