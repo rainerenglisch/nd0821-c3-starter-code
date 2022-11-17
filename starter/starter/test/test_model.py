54dc5805bce36c49c28bf0ba67bdd6a48460d82d
Rainer-Anton Englisch <rainerenglisch@users.noreply.github.com>
2022-11-17 11:08:24 +0100

    Update test_model.py

--- starter/starter/test/test_model.py
+++ starter/starter/test/test_model.py
@@ -6,8 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.exceptions import NotFittedError
 
-from ..ml.data import process_data
-from ..ml.model import compute_model_metrics, train_model, inference
+from starter.starter.ml.data import process_data
+from starter.starter.ml.model import compute_model_metrics, train_model, inference
 
 
 @pytest.fixture
